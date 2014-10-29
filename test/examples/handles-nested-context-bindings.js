7eadf25927279913e7079142d20343cf55153be0
Brian Donovan <donovan@squareup.com>
2014-10-29 11:37:10 -0700

    Fix binding `this` in nested arrow functions.

--- test/examples/handles-nested-context-bindings.js
+++ test/examples/handles-nested-context-bindings.js
@@ -3,7 +3,12 @@
 var obj = {
   method: function() {
     return () => (this, () => this);
+  },
+
+  method2: function() {
+    return () => () => this;
   }
 };
 
 assert.strictEqual(obj.method()()(), obj);
+assert.strictEqual(obj.method2()()(), obj);
